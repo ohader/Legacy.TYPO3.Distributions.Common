98d7b7ead72a0d83392904102b17a8d9dead1330
Oliver Hader <oliver@typo3.org>
2011-09-14 00:16:57 +0200

    [TASK] Phing ExecTask::execute() modifies class member variables
    Change-Id: I74b76faacba1c5176c712ad59a033b399bba9d6a

--- Tasks/Typo3ExecTask.php
+++ Tasks/Typo3ExecTask.php
@@ -8,24 +8,70 @@ class Typo3ExecTask extends ExecTask {
 	protected $retry = 0;
 
 	/**
+	 * @var integer
+	 */
+	protected $retryCount = 0;
+
+	/**
 	 * @param integer $retry
 	 */
 	public function setRetry($retry) {
 		$this->retry = (int) $retry;
 	}
 
-    /**
-     * Main method: wraps execute() command.
-     * @return void
-     */
+	/**
+	 * Main method: wraps execute() command.
+	 * @return void
+	 */
 	public function main() {
-		$retry = 0;
 		$return = $this->execute();
 
-		while ($return != 0 && $retry++ < $this->retry) {
-			$this->log('Retry #' . $retry . ' on previous failure...', Project::MSG_INFO);
-			$return = $this->execute();
+		while ($return != 0 && $this->retryCount++ < $this->retry) {
+			$return = $this->retry();
 		}
+	}
+
+	/**
+	 * Executes retry calls. The execute() method modifies class members,
+	 * that is exactly the reason for this additional method...
+	 *
+	 * @return integer Return code from execution.
+	 */
+	protected function retry() {
+		if ($this->dir !== null) {
+			if ($this->dir->isDirectory()) {
+				$currdir = getcwd();
+				@chdir($this->dir->getPath());
+			} else {
+				throw new BuildException("Can't chdir to:" . $this->dir->__toString());
+			}
+		}
+
+		$this->log('(Retry #' . $this->retryCount . ') Executing command: ' . $this->command);
+
+		if ($this->passthru) {
+			passthru($this->command, $return);
+		} else {
+			exec($this->command, $output, $return);
+		}
+
+		if ($this->dir !== null) {
+			@chdir($currdir);
+		}
+
+		foreach($output as $line) {
+			$this->log($line,  ($this->logOutput ? Project::MSG_INFO : Project::MSG_VERBOSE));
+		}
+
+		if ($this->returnProperty) {
+			$this->project->setProperty($this->returnProperty, $return);
+		}
+
+		if ($this->outputProperty) {
+			$this->project->setProperty($this->outputProperty, implode("\n", $output));
+		}
+
+		return $return;
 	}
 }
 ?>
